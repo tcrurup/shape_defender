266b0e173da47d8d8c6424caca36f0a5678a4e73
tcrurup <tcrurup@gmail.com>
2019-11-11 14:11:02 -0800

    add horizontal bouncing to the enemy balls

--- frontend/enemyUnit.js
+++ frontend/enemyUnit.js
@@ -8,13 +8,20 @@ class EnemyUnit extends GameUnit{
         this.y = 0
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
+        this.xVel = 5;
+        this.yVel = 5;
     }
 
     update(){
-        this.y += 5;
+        this.y += this.yVel;
+        this.x += this.xVel;
         this.element.style.top = `${this.y}px`;
+        this.element.style.left = `${this.x}px`;
         if(this.y > 800){
             this.isDestroyed = true
+        }
+        if(this.x >475 || this.x < 0){
+            this.xVel *= -1
         }
     }
 
