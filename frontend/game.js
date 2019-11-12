d4d35c6446221b3261dc179e4f432fd6f640172c
tcrurup <tcrurup@gmail.com>
2019-11-12 11:41:24 -0800

    remove all code from game class as it was unused

--- frontend/game.js
+++ frontend/game.js
@@ -1,40 +1,6 @@
 class Game{
 
-    constructor(controller){
-        this.allProjectiles = [];
-        this.gameController = controller
-        this.userUnit = new UserUnit(this)
+    constructor(){
     }
-
-    addElement(element){
-        this.controller.addElementToScreen(element)
-    }
-
-    update(){
-
-        for(let i = 0; i< this.allObjects.length; i++){
-            let object = this.allObjects[i]
-            if(object.isDestroyed){
-                this.allObjects = [...this.allObjects.slice(0,i) , ...this.allObjects.slice(i+1)]
-            } else {
-                object.update()
-            }
-        }
-    }
-
-    
-
-    get controller(){
-        return this.gameController
-    }
-
-    set controller(cont){
-        this.gameController = controller
-    }
-
-    get allPressedKeys(){
-        this.controller.allPressedKeys()
-    }
-
 
 }
