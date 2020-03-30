7c16a52a2698eaa1c8e16637c9557906499b74d0
tcrurup <tcrurup@gmail.com>
2020-03-30 07:47:40 -0700

    change format of score counter

--- frontend/scoreCounter.js
+++ frontend/scoreCounter.js
@@ -2,17 +2,40 @@ class ScoreCounter extends GameWindow{
 
     constructor(){
         super()
+        
+        this.currentScore = 0;
         this.element = document.createElement('div')
         this.element.id = ScoreCounter.elementId
-        this.currentScore = 0;
+
+        this._addTitle()
+        this._addCounter()
         this.hide()
     }
 
     set score(points){
-        this.element.innerHTML = points
+        this.element.querySelector('div#counter').innerHTML = points
     }
 
     static get elementId(){
         return 'scoreCounter'
+    }
+
+    _addTitle(){
+        let title = document.createElement('div')
+        title.innerHTML = "SCORE"
+
+        let divider = document.createElement('div')
+        divider.innerHTML = '-----------------------------------------'
+
+        this.element.appendChild(title)
+        this.element.appendChild(divider)
+    }
+
+    _addCounter(){
+        let counter = document.createElement('div')
+        counter.id = 'counter'
+        counter.innerHTML = '0'
+
+        this.element.appendChild(counter)
     }
 }
